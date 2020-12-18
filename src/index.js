e5fd2fe329a9ad14571647e4ff3af66f302c2ac4
xavieur <one@email.com>
2020-12-18 12:47:57 +0100

    feat: Header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './sass/style.scss'
 
@@ -20,20 +20,33 @@ const CancionesFAQ = () => (
     <div>Preguntas frecuentes sobre Canciones</div>
 )
 
-const Canciones404 = ()=>(
-    <div>Oooops! No hay canciones por aquí!</div>
+const Canciones404 = () => (
+    <div>Oooops! No hay canciones por aquí! <Link to='/'>Prueba en la página de Inicio</Link></div>
 )
 
-const routes = (
+const CancionesHeader = () => (
+    <header>
+        <h1>Canciones</h1>
+        <Link to='/'>Inicio</Link>
+        <Link to='/create'>Crear</Link>
+        <Link to='/edit'>Edit</Link>
+        <Link to='/faq'>FAQ</Link>
+    </header>
+)
+
+const CancionesApp = () => (
     <BrowserRouter>
-        <Switch>
-            <Route path='/' component={CancionesHome} exact={true} />
-            <Route path='/create' component={CancionesCreate} />
-            <Route path='/edit' component={CancionesEdit} />
-            <Route path='/faq' component={CancionesFAQ} />
-            <Route component={Canciones404} />
-        </Switch>
+        <div>
+            <CancionesHeader />
+            <Switch>
+                <Route path='/' component={CancionesHome} exact={true} />
+                <Route path='/create' component={CancionesCreate} />
+                <Route path='/edit' component={CancionesEdit} />
+                <Route path='/faq' component={CancionesFAQ} />
+                <Route component={Canciones404} />
+            </Switch>
+        </div>
     </BrowserRouter>
 )
 
-ReactDOM.render(routes, document.querySelector('#appRoot'))
+ReactDOM.render(<CancionesApp />, document.querySelector('#appRoot'))
